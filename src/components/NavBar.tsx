1d8fbf52fe44714fd75ee614df627b5ce549dbf5
Hatim Shakir <hatimshakir9179@gmail.com>
2024-01-02 22:21:08 +0530

    Add: dummy search bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,18 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justify="space-between" padding="10px">
+    <HStack padding="10px">
       <Image
         src={logo}
         boxSize="60px"
         borderRadius="10px"
         background="transparent"
       />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
