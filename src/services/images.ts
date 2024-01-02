2b79e004d6eb1cdab39e0ea4e3888a96f6118270
Hatim Shakir <hatimshakir9179@gmail.com>
2024-01-02 21:55:14 +0530

    Fix: handle no image games

--- src/services/images.ts
+++ src/services/images.ts
@@ -1,5 +1,5 @@
 const getCroppedUImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return "https://placehold.co/600x400";
 
   const index = url.indexOf("media/") + "media/".length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
